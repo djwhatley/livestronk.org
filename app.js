f5e36c66345db72846aa1d6f99f7606608f94207
Daniel Whatley <danieljwhatley@gmail.com>
2014-02-19 14:34:57 +0000

    Kongstruction has begun

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-	res.send("#LIVESTRONK");
+	//res.send("#LIVESTRONK");
+	res.render('index.jade')
 });
 
-app.listen(8080)
+app.listen(8080);
 console.log("Listening on port 8080");
