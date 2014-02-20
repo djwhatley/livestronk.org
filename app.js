024a5f19c9a233782713140501dc22fb74f40bf2
Daniel Whatley <danieljwhatley@gmail.com>
2014-02-20 00:22:16 +0000

    Testing supervisor for reload

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ app.get('/', function(req, res) {
 	res.render('index.jade')
 });
 
+app.get('/test', function(req, res) {
+	res.send('Success')
+});
+
 app.listen(8080);
 console.log("Listening on port 8080");
